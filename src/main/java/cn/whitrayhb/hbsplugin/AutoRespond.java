9a731dcf1edaa3426a47d9c149a068f7a589501c
Akiba So <37971883+akibaso@users.noreply.github.com>
2022-09-04 12:03:27 +0800

    Update AutoRespond.java

--- src/main/java/cn/whitrayhb/hbsplugin/AutoRespond.java
+++ src/main/java/cn/whitrayhb/hbsplugin/AutoRespond.java
@@ -23,8 +23,8 @@ public class AutoRespond {
         eventChannel.subscribeAlways(GroupMessageEvent.class, g -> {
             String message = g.getMessage().contentToString();
             if (message.startsWith("摸")) return;
-            if (message.startsWith("哼哼")||message.endsWith("哼哼哼")){
-                int count=searchCount("哼",message);
+            if (message.startsWith("哼哼")||message.endsWith("哼哼")){
+                int count = searchCount("哼", message);
                 String content="啊啊啊啊啊啊";
                 for (int i=0;i<count;i++){
                     content+="啊";
@@ -32,7 +32,7 @@ public class AutoRespond {
                 g.getGroup().sendMessage(content);
             }
             if (message.trim().equals("114514")) g.getGroup().sendMessage("1919810");
-            if (message.trim().equals("1919810")) g.getGroup().sendMessage("114514");
+            if (message.trim().equals("1919810")) g.getGroup().sendMessage("114514");.
             else if (message.contains("撅鸽子")||message.contains("撅爆鸽子")) g.getGroup().sendMessage("我先把你撅爆再说（恼）");
             else if (message.contains("鸽子")) g.getGroup().sendMessage("谁叫我");
             else if (message.contains("咕咕咕")) g.getGroup().sendMessage("咕咕咕~");
